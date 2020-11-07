640d3e170980b11f729961479d67b12689a09478
Silent <zdanio95@gmail.com>
2020-11-07 23:06:06 +0100

    Bump up version

--- source/VersionInfo.lua
+++ source/VersionInfo.lua
@@ -1,7 +1,7 @@
 defines { 
     "rsc_FullName=\"SilentPatch for Mass Effect\"",
     "rsc_MinorVersion=0",
-    "rsc_RevisionID=1",
+    "rsc_RevisionID=2",
     "rsc_BuildID=0",
     "rsc_Copyright=\"2020\"",
     "rsc_UpdateURL=\"https://github.com/CookiePLMonster/SilentPatchME/releases\""
